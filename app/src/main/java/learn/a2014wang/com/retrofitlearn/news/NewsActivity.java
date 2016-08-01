402a7454855972e0edd66d6a8db4d81350b5c7bb
1242128273wangpeng <1242128273@qq.com>
2016-08-01 23:06:03 +0800

    add listview in NewsActivity

--- app/src/main/java/learn/a2014wang/com/retrofitlearn/news/NewsActivity.java
+++ app/src/main/java/learn/a2014wang/com/retrofitlearn/news/NewsActivity.java
@@ -2,18 +2,20 @@ package learn.a2014wang.com.retrofitlearn.news;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import learn.a2014wang.com.retrofitlearn.R;
 import learn.a2014wang.com.retrofitlearn.news.domain.Constants;
 import learn.a2014wang.com.retrofitlearn.news.domain.NewsRetrofitService;
 
 public class NewsActivity extends AppCompatActivity {
+    private ListView mlistview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
-     NewsRetrofitService.getNewRetrofitInstance(this,NewsRetrofitService.SIMPLE).getNewsRxjava_A(Constants.APIKEY);
+        NewsRetrofitService.getNewRetrofitInstance(this, NewsRetrofitService.SIMPLE).getNewsRxjava_A(Constants.APIKEY);
 //        NewsRetrofitService.getNewRetrofitInstance(this,NewsRetrofitService.HAVEHEADER).getNewsRxjava_B();
 //     NewsRetrofitService.getNewRetrofitInstance(this,NewsRetrofitService.HAVEHEADER).getNews_C();
     }
